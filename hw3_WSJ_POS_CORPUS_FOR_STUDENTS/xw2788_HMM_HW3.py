ba7c10bf7558c333fc69f3d148f61c6826378ac6
Harry <xiaohanwu12@gmail.com>
2022-09-22 23:32:19 -0400

    hw3: add tagging functions

--- hw3_WSJ_POS_CORPUS_FOR_STUDENTS/xw2788_HMM_HW3.py
+++ hw3_WSJ_POS_CORPUS_FOR_STUDENTS/xw2788_HMM_HW3.py
@@ -5,6 +5,18 @@ def read_input(filename):
         context = f.read()
     return context
 
+def read_with_lines(filename):
+    context = []
+    line = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for token in f.read().split("\n"):
+            if token == '' and len(line) > 0:
+                context.append(line)
+                line = []
+            else:
+                line.append(token)
+    return context
+
 def calc_probability(freq_dict):
     total = 0
     prob_dict = {}
@@ -21,6 +33,8 @@ def parse_input(context):
     pre_state = "Begin_Sent"
     for line in context.split('\n'):
         if len(line.split('\t')) < 2:
+            if pre_state == "Begin_Sent":
+                continue
             state = 'End_Sent'
             state_freq[pre_state] = state_freq.get(pre_state, {})
             state_freq[pre_state][state] = state_freq[pre_state].get(state, 0) + 1
@@ -42,11 +56,64 @@ def parse_input(context):
         state_prob[k] = calc_probability(d)
     return word_prob, state_prob, word_list
 
+def viterbi_HMM_POS_tagger(sentence, likelihood, probability, word_list):
+    m = len(likelihood) + 2
+    n = len(sentence) + 2
+    table = {k: [0 for _ in range(n)] for k in probability}
+    table['End_Sent'] = [0 for _ in range(n)]
+    pointer = {k: ["" for _ in range(n)] for k in probability}
+    pointer['End_Sent'] = ["" for _ in range(n)]
+    table['Begin_Sent'][0] = 1
+    pre_states = ["Begin_Sent"]
+    for col in range(1, n):
+        if col == n - 1:
+            state = 'End_Sent'
+            for pre_state in pre_states:
+                score = table[pre_state][col-1] * probability[pre_state].get(state, 0)
+                if score > table[state][col]:
+                    table[state][col] = score
+                    pointer[state][col] = pre_state
+        else:
+            word = sentence[col-1]
+            states = set()
+            for pre_state in pre_states:
+                for state in probability[pre_state]:
+                    if state == 'End_Sent':
+                        continue
+                    states.add(state)
+                    score = table[pre_state][col-1] * probability[pre_state].get(state, 0) * likelihood[state].get(word, 0.1)
+                    if score > table[state][col]:
+                        table[state][col] = score
+                        pointer[state][col] = pre_state
+            pre_states = list(states)
+    
+    tagging = []
+    nxt_state = 'End_Sent'
+    for idx in range(n-1, 1, -1):
+        tagging = [pointer[nxt_state][idx]] + tagging
+        nxt_state = pointer[nxt_state][idx]
+    return tagging
+
+
+def write_output(filename, context):
+    with open(filename, "w", encoding='utf-8') as f:
+        for line in context:
+            f.write(line + '\n')
 
 def main():
-    context = read_input('WSJ_24.pos')
+    training_txt = 'WSJ_02-21.pos'
+    context = read_input(training_txt)
     likelihood, probability, word_list = parse_input(context)
-    
+
+    testing_txt = 'WSJ_24.words'
+    context = read_with_lines(testing_txt)
+    sent_tag = []
+    for sentence in context:
+        res = viterbi_HMM_POS_tagger(sentence, likelihood, probability, word_list)    
+        sent_tag.extend([sent + '\t' + tag for sent, tag in zip(sentence, res)])
+        sent_tag.append('')
+    output_txt = 'submission.pos'
+    write_output(output_txt, sent_tag)
 
 if __name__ == "__main__":
     main()
