4241d57165c2c23513e20abd0b54ca78f75d5f11
Harry <xiaohanwu12@gmail.com>
2022-09-24 15:53:15 -0400

    refactor: renew all

--- hw3_WSJ_POS_CORPUS_FOR_STUDENTS/xw2788_HMM_HW3.py
+++ hw3_WSJ_POS_CORPUS_FOR_STUDENTS/xw2788_HMM_HW3.py
@@ -26,74 +26,126 @@ def calc_probability(freq_dict):
         prob_dict[k] = v/total
     return prob_dict
 
+def generate_unknown_word(word_count, word_freq):
+    for word, count in list(word_count.items()):
+        if count == 1:
+            word_count.pop(word)
+            word_count['UNKNOWN_WORD'] = word_count.get('UNKNOWN_WORD', 0) + 1
+            for state, words in list(word_freq.items()):
+                if word in words.keys():
+                    word_freq[state].pop(word)
+                    word_freq[state]['UNKNOWN_WORD'] = word_freq[state].get('UNKNOWN_WORD', 0) + 1
+    return word_count, word_freq
+
 def parse_input(context):
-    word_freq = {}
-    state_freq = {}
-    word_list = set()
+    state_word_freq = {}
+    state_state_freq = {}
+    word_count = {}
+    state_list = set()
     pre_state = "Begin_Sent"
     for line in context.split('\n'):
         if len(line.split('\t')) < 2:
             if pre_state == "Begin_Sent":
                 continue
             state = 'End_Sent'
-            state_freq[pre_state] = state_freq.get(pre_state, {})
-            state_freq[pre_state][state] = state_freq[pre_state].get(state, 0) + 1
+            state_list.add(state)
+            state_state_freq[pre_state] = state_state_freq.get(pre_state, {})
+            state_state_freq[pre_state][state] = state_state_freq[pre_state].get(state, 0) + 1
             pre_state = 'Begin_Sent'
         else:
             [word, state] = line.split('\t')
-            word_list.add(word)
-            word_freq[state] = word_freq.get(state, {})
-            word_freq[state][word] = word_freq[state].get(word, 0) + 1
+            word = word.lower()
+            word_count[word] = word_count.get(word, 0) + 1
+            state_word_freq[state] = state_word_freq.get(state, {})
+            state_word_freq[state][word] = state_word_freq[state].get(word, 0) + 1
 
-            state_freq[pre_state] = state_freq.get(pre_state, {})
-            state_freq[pre_state][state] = state_freq[pre_state].get(state, 0) + 1
+            state_list.add(state)
+            state_state_freq[pre_state] = state_state_freq.get(pre_state, {})
+            state_state_freq[pre_state][state] = state_state_freq[pre_state].get(state, 0) + 1
             pre_state = state
-    word_prob = {}
-    state_prob = {}
-    for k, d in word_freq.items():
-        word_prob[k] = calc_probability(d)
-    for k, d in state_freq.items():
-        state_prob[k] = calc_probability(d)
-    return word_prob, state_prob, word_list
+
+    word_count, state_word_freq = generate_unknown_word(word_count, state_word_freq)
+
+    state_word_prob = {}
+    state_state_prob = {}
+    for state in state_word_freq:
+        total = 0
+        for freq in state_word_freq[state].values():
+            total += freq
+        for word in word_count:
+            state_word_prob[state] = state_word_prob.get(state, {})
+            if word in state_word_freq[state]:
+                state_word_prob[state][word]= state_word_freq[state].get(word, 0) / total
+            else:
+                state_word_prob[state][word] = 0
+
+    for pre_state in state_state_freq:
+        total = 0
+        for freq in state_state_freq[pre_state].values():
+            total += freq
+        for state in state_list:
+            state_state_prob[pre_state] = state_state_prob.get(pre_state, {})
+            if state in state_state_freq[pre_state]:
+                state_state_prob[pre_state][state] = state_state_freq[pre_state].get(state, 0) / total
+            else:
+                state_state_prob[pre_state][state] = 0
+    return state_word_prob, state_state_prob, list(word_count.keys())
 
 def viterbi_HMM_POS_tagger(sentence, emissions, transitions, word_list):
-    n = len(sentence) + 2
-    table = {k: [0 for _ in range(n)] for k in transitions}
-    table['End_Sent'] = [0 for _ in range(n)]
-    pointer = {k: ["" for _ in range(n)] for k in transitions}
-    pointer['End_Sent'] = ["" for _ in range(n)]
-    table['Begin_Sent'][0] = 1000
-    pre_states = ["Begin_Sent"]
-    for col in range(1, n):
-        if col == n - 1:
-            state = 'End_Sent'
-            for pre_state in pre_states:
-                score = table[pre_state][col-1] * transitions[pre_state].get(state, 0)
-                if score > table[state][col]:
-                    table[state][col] = score
-                    pointer[state][col] = pre_state
-        else:
-            word = sentence[col-1]
-            states = set()
-            for pre_state in pre_states:
-                for state in transitions[pre_state]:
-                    if state == 'End_Sent':
-                        continue
-                    states.add(state)
-                    score = table[pre_state][col-1] * transitions[pre_state].get(state, 0) * emissions[state].get(word, 0)
-                    if table[pre_state][col-1] > 0 and transitions[pre_state].get(state, 0)>0 and emissions[state].get(word, 0)>0 and score == 0:
-                        score = min(table[pre_state][col-1],  transitions[pre_state].get(state, 0), emissions[state].get(word, 0))
-                    if score > table[state][col]:
-                        table[state][col] = score
-                        pointer[state][col] = pre_state
-            pre_states = list(states)
+    tables = [{'Begin_Sent': 1}]
+    pointers = [{'Begin_Sent': None}]
+    for word in sentence:
+        word = word.lower()
+        table = {}
+        pointer = {}
+        for pre_state in pointers[-1].keys():
+            for state in transitions[pre_state]:
+                if state == 'End_Sent':
+                    continue
+                probability = transitions[pre_state].get(state, 0)
+                likelihood = emissions[state].get(word, 0.001)
+                # if not likelihood:
+                #     likelihood = emissions[state].get("UNKNOWN_WORD", 0.001)
+                pre_prob = tables[-1][pre_state]
+                score = pre_prob * probability * likelihood
+                max_score = table.get(state, 0)
+                # print(pre_state, state, word)
+                # print(pre_prob, probability, likelihood)
+                # print(score, max_score)
+                # for k, v in pointer.items():
+                #     print(f"{k}->{v}", end='\t')
+                # input()
+                if score > table.get(state, -1):
+                    table[state] = score
+                    pointer[state] = pre_state
+        tables.append(table)
+        pointers.append(pointer)
+        # print(table)
+        # for k, v in pointer.items():
+        #     print(f"{k}->{v}")
+        # input()
+    state = 'End_Sent'
+    table = {}
+    pointer = {}
+    for pre_state in pointers[-1].keys():    
+        probability = transitions[pre_state].get(state, 0)
+        pre_prob = tables[-1][pre_state]
+        score = pre_prob * probability
+        if score > table.get(state, -1):
+            table[state] = score
+            pointer[state] = pre_state
+    tables.append(table)
+    pointers.append(pointer)
 
     tagging = []
-    cur_state = 'End_Sent'
-    for idx in range(n-1, 1, -1):
-        pre_state = pointer[cur_state][idx]
+    state = 'End_Sent'
+    # for line in pointers:
+    #     print(line)
+    # print()
+    for idx in range(len(pointers)-1, 1, -1):
+        pre_state = pointers[idx][state]
         tagging = [pre_state] + tagging
-        cur_state = pre_state
+        state = pre_state
     return tagging
 
 
@@ -109,13 +161,14 @@ def main():
         context += read_input(txt)
     emissions, transitions, word_list = parse_input(context)
 
-    testing_txt = 'WSJ_24.words'
+    testing_txt = 'WSJ_23.words'
     context = read_with_lines(testing_txt)
     sent_tag = []
     for sentence in context:
         res = viterbi_HMM_POS_tagger(sentence, emissions, transitions, word_list)
         sent_tag.extend([sent + '\t' + tag for sent, tag in zip(sentence, res)])
         sent_tag.append('')
+        # break
     output_txt = 'submission.pos'
     write_output(output_txt, sent_tag)
 
