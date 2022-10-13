c13222972e8daebfb6a29b364ae0f896e13ab44b
Harry <xiaohanwu12@gmail.com>
2022-10-13 10:49:24 -0400

    hw5: add previous and following 2 words

--- hw5_Noun_Group_Tagger/hw5.py
+++ hw5_Noun_Group_Tagger/hw5.py
@@ -7,57 +7,56 @@ email: xiaohanwu12@gmail.com
 import sys
 
 
-BEGIN_SENT = "Begin_Sent"
-END_SENT = "End_Sent"
 def parse_input(filename):
     context = []
     with open(filename, 'r', encoding='utf-8') as f:
-        # add begin sentence at starting
-        context.append([BEGIN_SENT])
         for line in f.readlines():
             line = line.rstrip('\n')
-            # print(f"'{line}'", end=', ')
-            if line == '':
-                # add to context to indicate an empty line
-                if context[-1] != [BEGIN_SENT]:
-                    context.append([END_SENT])
-                # context.apend([''])
-                continue
-            if context[-1] == [END_SENT]:
-                context.append([BEGIN_SENT])
             context.append(line.split('\t'))
-        # add end sentence at the end
-        if context[-1] != [END_SENT]:
-            context.append([END_SENT])
     return context
 
 def write_output(filename, context):
     with open(filename, "w", encoding='utf-8') as f:
         for line in context:
-            if line == BEGIN_SENT:
-                line = ''
-            if line == END_SENT:
-                continue
             f.write(f"{line}\n")
-        f.write('\n')
 
-def parse_context(context):
+def parse_word(word):
+    return word.lower()
+
+def parse_context(context, training=False):
     features = []
-    for line in context:
+    for i, line in enumerate(context):
         feature = []
+        previous_line = context[i-1] if i > 0 and len(context[i-1]) > 1 else None
+        following_line = context[i+1] if i < len(context) - 1 and len(context[i+1]) > 1 else None
+        previous2_line = context[i-2] if previous_line and i > 1 and len(context[i-2]) > 1 else None
+        following2_line = context[i+2] if following_line and i < len(context) - 2 and len(context[i+2]) > 1 else None
         if len(line) < 2:
             feature.append(line[0])
         else:
-            word = line[0]
-            feature.append(word)
+            feature.append(parse_word(line[0])) # add word
             feature.append(f"POS={line[1]}")
-            feature.append(line[-1])
+            if previous_line:
+                feature.append(f"previous_word={parse_word(previous_line[0])}")
+                feature.append(f"previous_POS={previous_line[1]}")
+            if following_line:
+                feature.append(f"following_word={parse_word(following_line[0])}")
+                feature.append(f"following_POS={following_line[1]}")
+            if previous2_line:
+                feature.append(f"previous2_word={parse_word(previous2_line[0])}")
+                feature.append(f"previous2_POS={previous2_line[1]}")
+            if following2_line:
+                feature.append(f"following2_word={parse_word(following2_line[0])}")
+                feature.append(f"following2_POS={following2_line[1]}")
+            feature.append(f"previous_BIO=@@")
+            if training:
+                feature.append(line[-1])
         features.append('\t'.join(feature))
     return features
 
 def generate_features(input_file, output_file):
     context = parse_input(input_file)
-    features = parse_context(context)
+    features = parse_context(context, "training" in output_file)
     write_output(output_file, features)
 
 def main():
