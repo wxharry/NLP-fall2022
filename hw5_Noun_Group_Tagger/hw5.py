736f9518045570d9492a9143ae30abcec8fc626b
Harry <xiaohanwu12@gmail.com>
2022-10-14 22:17:10 -0400

    hw5: improve f1 to 9.18

--- hw5_Noun_Group_Tagger/hw5.py
+++ hw5_Noun_Group_Tagger/hw5.py
@@ -4,8 +4,13 @@ NYU ID: xw2788
 email: xiaohanwu12@gmail.com
 """
 
+from string import punctuation
 import sys
-
+import nltk
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+sno = nltk.stem.SnowballStemmer('english')
+lemma = nltk.wordnet.WordNetLemmatizer()
 
 def parse_input(filename):
     context = []
@@ -20,11 +25,9 @@ def write_output(filename, context):
         for line in context:
             f.write(f"{line}\n")
 
-def parse_word(word):
-    return word.lower()
-
 def parse_context(context, training=False):
     features = []
+    offset = 0
     for i, line in enumerate(context):
         feature = []
         previous_line = context[i-1] if i > 0 and len(context[i-1]) > 1 else None
@@ -33,22 +36,36 @@ def parse_context(context, training=False):
         following2_line = context[i+2] if following_line and i < len(context) - 2 and len(context[i+2]) > 1 else None
         if len(line) < 2:
             feature.append(line[0])
+            offset = 0
         else:
-            feature.append(parse_word(line[0])) # add word
+            word = line[0]
+            feature.append(word) # add word
             feature.append(f"POS={line[1]}")
+            feature.append(f"capitalization={word == word.capitalize()}")
+            feature.append(f"lowercase={word.islower()}")
+            feature.append(f"offset={offset+1}")
+            offset += 1
+            feature.append(f"STEMMED={sno.stem(word)}")
             if previous_line:
-                feature.append(f"previous_word={parse_word(previous_line[0])}")
+                word = previous_line[0]
+                feature.append(f"previous_word={word}")
                 feature.append(f"previous_POS={previous_line[1]}")
+                if training:
+                    feature.append(f"previous_BIO={previous_line[-1]}")
+                else:
+                    feature.append(f"previous_BIO=@@")
             if following_line:
-                feature.append(f"following_word={parse_word(following_line[0])}")
+                word = following_line[0]
+                feature.append(f"following_word={word}")
                 feature.append(f"following_POS={following_line[1]}")
             if previous2_line:
-                feature.append(f"previous2_word={parse_word(previous2_line[0])}")
+                word = previous2_line[0]
+                feature.append(f"previous2_word={word}")
                 feature.append(f"previous2_POS={previous2_line[1]}")
             if following2_line:
-                feature.append(f"following2_word={parse_word(following2_line[0])}")
-                feature.append(f"following2_POS={following2_line[1]}")
-            feature.append(f"previous_BIO=@@")
+                word = following2_line[0]
+                feature.append(f"following2_word={word}")
+                feature.append(f"following2_POS={following2_line[1]}") 
             if training:
                 feature.append(line[-1])
         features.append('\t'.join(feature))
